c5fc25ac9f5b360ca529c99f652323d198f19ee2
Steven <106054873+stevenpartida@users.noreply.github.com>
2024-08-19 22:12:15 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+import discord
+import os
+from dotenv import load_dotenv
+import webserver
+
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
+
+intents = discord.Intents.default()
+intents.messages = True 
+intents.message_content = True  
+
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print(f"{client.user} has connected to Discord! again...")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    print(f"Received message: {message.content}") 
+    
+    if message.content.startswith('https://x.com/') or message.content.startswith('https://twitter.com/'):
+        link = message.content
+        if link.startswith('https://twitter.com/'):
+            fx_link = link.replace('https://twitter.com/', 'https://fxtwitter.com/')
+        elif link.startswith('https://x.com/'):
+            fx_link = link.replace('https://x.com/', 'https://fxtwitter.com/')
+
+        await message.delete()
+        await message.channel.send(f"{fx_link} \nFrom: {message.author.mention}")
+
+webserver.keep_alive()
+client.run(TOKEN)
